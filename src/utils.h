13881337c8af664c608e14ce924e7f2f369d3029
luigi.brancati <luigi.brancati93@gmail.com>
2023-02-11 15:59:11 +0100

    Split code between multiple headers, added new algorithms

---
+++ src/utils.h
@@ -0,0 +1,25 @@
+#ifndef _POKER_UTILS_H
+#define _POKER_UTILS_H
+
+#include "global.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+template<class T>
+void sort_cards(T& cards) {
+    std::sort(cards.begin(), cards.end(), [](Card& a, Card& b){return (a.value<=b.value);});
+}
+
+string round_float(float a,int num_decimals) {
+    int temp = round(a*pow(10,num_decimals));
+    string total_number = to_string((float) temp/(pow(10,num_decimals)));
+    return total_number.substr(0,total_number.find(".")+2);
+}
+
+#endif
